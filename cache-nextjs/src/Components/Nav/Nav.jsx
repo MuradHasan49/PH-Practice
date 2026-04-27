9ed741b0f1eb83777f6938ac5e29eef45e25d208
Murad Hasan <muradhasanshuvo00@gmail.com>
2026-04-27 16:28:40 +0600

    create navbar and router

---
+++ cache-nextjs/src/Components/Nav/Nav.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Nav = () => {
+  return (
+    <nav className="p-4 bg-gray-100 ">
+      <ul className="flex items-center justify-center gap-6 list-none ">
+        <li>
+          <Link href="/" className="text-gray-800 hover:text-blue-600 transition-colors">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/prodect" className="text-gray-800 hover:text-blue-600 transition-colors">
+           Product
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact" className="text-gray-800 hover:text-blue-600 transition-colors">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
