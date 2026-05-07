ee891d71e26544b9004b2624daeb27f8a9599f2f
Murad Hasan <muradhasanshuco00@gmail.com>
2026-05-07 20:10:26 +0600

    add new file

---
+++ hero-ui/src/Components/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi eius, et amet iusto nemo, provident iste, sapiente at ut voluptatum excepturi est.
+    </div>
+  )
+}
+
+export default Test
--- hero-ui/src/lib/getData.js
+++ hero-ui/src/lib/getData.js
@@ -5,6 +5,7 @@ export const getData = async () => {
     return fackdata;
 }
 
+
 export const setData= async (newTask) => {
     newTask.id === fackdata.length + 1;
     fackdata.unshift(newTask);
